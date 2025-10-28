f8a6d9e8ff37d4fa79844e2797ffee81f6d7879c
Chizoba Jude Ojini <judoc96@gmail.com>
2025-10-28 17:58:27 +0100

    chore: cleanup favicon integration and use updated icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	const { children } = $props();
 </script>
 
 <svelte:head>
-	<title>Lienzo - Create Beautiful Documents</title>
+	<title>Lienzo - A modern document builder</title>
 	<meta
 		name="description"
 		content="A versatile document builder for creating resumes, invoices, letters, and more. Build beautiful, professional documents with our intuitive drag-and-drop canvas."
 	/>
-	<link rel="icon" href={favicon} />
 	<link rel="stylesheet" href="/fonts/fonts.css" />
 </svelte:head>
 
